c42e653d47d19469e95dee55e9a111b878dd53c6
unknown <cchester1@angelo.edu>
2021-11-24 18:47:35 -0600

    progressed made in sequencing the game. rewriting the promise of the fight function to be an anonymous function fixed the problem with the fight for loop

--- startGame.js
+++ startGame.js
@@ -8,6 +8,7 @@ const fight = require('./utils/fight')
 // player 
 let Freyja = 'Freyja'
 let playerHealth = 90
+
 // enemy 
 let enemyNames = []
 
@@ -24,18 +25,20 @@ const startGame = () => {
             if (start_game == true) {
                 for (let i = 0; i < enemyNames.length; i++) {
                     if (playerHealth > 0) {
+                        console.log(playerHealth)
                         if (i > 0) {
                             playerHealth += 10
+                            console.log('player health is ' + playerHealth )
 
                             console.log(`Your champion ${Freyja} has ${playerHealth} health points left! She will not be slain!`)
 
                             let nextFighter = enemyNames[i]
 
                             fight(nextFighter, playerHealth)
-                        } 
-                        let nextFighter = enemyNames[i]
-                            
-                        fight(nextFighter, playerHealth)
+                        } else if (i === 0) {
+                            let nextFighter = enemyNames[i]
+                            fight(nextFighter, playerHealth)
+                        }
                     } else {
                         endGame(startGame())
                     }
--- utils/fight.js
+++ utils/fight.js
@@ -1,50 +1,53 @@
 const inquirer = require('inquirer')
 
-const adj1Array = ['hefty', 'furious', 'fiery', 'reddened']
-const adj1 = adj1Array[Math.floor(Math.random() * adj1Array.length)]
-const verbArray = ['leaped', 'twisted', 'formed']
-const verb = verbArray[Math.floor(Math.random() * verbArray.length)]
-const prepArray = ['beneath', 'under', 'within']
-const preposition = prepArray[Math.floor(Math.random() * prepArray.length)]
-const nounArray = ['bush', 'maple tree', 'foggy mist', 'black bog']
-const noun = nounArray[Math.floor(Math.random() * nounArray.length)]
-
-const playerAttackBonus = [1, 2, 3, 4, 5]
-let playerAttack = 5 + Math.floor(Math.random() * playerAttackBonus.length)
-let eHealth = 59 + Math.floor(Math.random() + 5)
-let enemyAttack = 7
-
 const fight = (fighter, pHealth) => {
+    const adj1Array = ['hefty', 'furious', 'fiery', 'reddened']
+    const adj1 = adj1Array[Math.floor(Math.random() * adj1Array.length)]
+    const verbArray = ['leaped', 'twisted', 'formed']
+    const verb = verbArray[Math.floor(Math.random() * verbArray.length)]
+    const prepArray = ['beneath', 'under', 'within']
+    const preposition = prepArray[Math.floor(Math.random() * prepArray.length)]
+    const nounArray = ['bush', 'maple tree', 'foggy mist', 'black bog']
+    const noun = nounArray[Math.floor(Math.random() * nounArray.length)]
+
+    const playerAttackBonus = [1, 2, 3, 4, 5]
+    let playerAttack = 5 + Math.floor(Math.random() * playerAttackBonus.length)
+    let eHealth = 59 + Math.floor(Math.random() + 5)
+    let enemyAttack = 7
+
+
     console.log(`${fighter}! A ${adj1} opponent ${verb} out from ${preposition} a ${noun}`)
 
-    const attackPrompt = inquirer.prompt({
-        type: 'input',
+    enemyHealth = 59 + Math.floor(Math.random() + 5)
+
+    inquirer.prompt({
+        type: 'confirm',
         name: 'attack_flee',
-        message: 'Will you attack the enemy or flee the scene? Reply "attack" to attack or "flee" to flee.'
+        message: 'Will you attack the enemy or flee the scene?'
     })
-        .then(() => {
-            if (attackPrompt === 'attack') {
-                while (pHealth > 0 || eHealth > 0) {
-                    eHealth = eHealth - playerAttack
+        .then(function ({ attack_flee }) {
+                if (attack_flee == true) {
+                    while (pHealth > 0 || eHealth > 0) {
+                        eHealth = eHealth - playerAttack
 
-                    console.log(`${fighter} took ${playerAttack} points of damage!`)
+                        console.log(`${fighter} took ${playerAttack} points of damage! You slash at him with your sword!`)
 
-                    if(eHealth <= 0) {
-                        console.log(`The ${adj1} ${fighter} has been defeated!`)
-                        break;
-                    }
+                        if (eHealth <= 0) {
+                            console.log(`The ${adj1} ${fighter} has been defeated!`)
+                            break
+                        }
 
-                    pHealth = pHealth - enemyAttack
+                        pHealth = pHealth - enemyAttack
 
-                    console.log(`You took 7 points of damage! The enemies consistency is worrying.`)
+                        console.log(`You took 7 points of damage! The enemies consistency is worrying.`)
 
-                    if(pHealth <= 0) {
-                        console.log('Your champion Freyja ascends to Valhalla where she awaits an eternity of glorious battles and infitely full jugs of wine')
-                        break;
+                        if (pHealth <= 0) {
+                            console.log('Your champion Freyja ascends to Valhalla where she awaits an eternity of glorious battles and infitely full jugs of wine')
+                            break
+                        }
                     }
                 }
-            }
-        })
+            })
 }
 
 module.exports = fight;
